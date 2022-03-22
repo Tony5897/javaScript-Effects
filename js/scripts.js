6c12b87b8578c9ce80de805b295dc14aacfe77e9
tonym <tony.martinez5897@gmail.com>
2022-03-22 10:55:21 -0700

    Added jquery and css functionality.
    co-authored-by: Evgeniya Meshuris <em@theemm.com>

--- js/scripts.js
+++ js/scripts.js
@@ -2,5 +2,9 @@ $(document).ready(function() {
   $(".clickable").click(function() {
     $("#walrus-showing").fadeToggle();
     $("#walrus-hidden").slideToggle();
-  });
+    });
+  $(".clickable2").click(function() {
+    $("#dolphin").fadeToggle();
+    $("#dolphin").slideToggle();
+    }); 
 });
